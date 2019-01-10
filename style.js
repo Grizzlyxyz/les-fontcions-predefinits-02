25a66d04029803051db0b0b9235f6f42c8800eb6
Grizzly <grizzlyxyz@gmail>
2019-01-10 16:49:53 +0100

    exo007

--- style.js
+++ style.js
@@ -1,13 +1,51 @@
 // Exo 01
 // Créer une variable du nom de number contenant 3.7
-// Affiché cette variable arrondie à son entier superieur// Exo 02
-// Afficher la variable number à son entier inférieur// Exo 03
+// Affiché cette variable arrondie à son entier superieur
+// Exo 02
+// Afficher la variable number à son entier inférieur
+// Exo 03
 // Créer deux variables number01 et number02
 // L'une valant 4 et l'une valant 7
 // Avec l'aide d'une function retourner le plus grand// Exo 04
-// Avec l'aide d'une function et des variables de l'exercice 03 retourner le plus petit// Exo05
+// Avec l'aide d'une function et des variables de l'exercice 03 retourner le plus petit
+// Exo05
 // Créer trois variables non entier avec comme nom et valeur :
 // nombreNonEntier01 = 54.7
 // nombreNonEntier02 = 147.1
 // nombreNonEntier03 = 14.4
 // Les mettres a leur entier le plus grand et retourner le plus grand
+
+//Exo 1
+let number = "3.7";
+console.log(Math.ceil(number));
+console.log("   ")
+
+//Exo 2 
+
+console.log(Math.floor(number))
+
+//Exo 3
+
+let number01 = "4";
+let number02 = "7";
+
+console.log(Math.max(number01, number02));
+
+//Exo 4 
+
+console.log(Match.min(number01, number02));
+
+//Exo 5
+
+console.log(Math.max(Math.ceil(nombreNonEntier01),Math.ceil(nombreNonEntier02), Math.ceil(nombreNonEntier03)));
+
+//Exo 6
+
+console.log("Exo 6");
+
+console.log(Math.random(13,500)*100);
+
+
+
+
+
