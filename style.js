f145a82972f755f76415ea63080253e1c9b0c9fc
Grizzly <grizzlyxyz@gmail>
2019-01-10 15:58:12 +0100

    consigne exo

--- style.js
+++ style.js
@@ -1,6 +1,13 @@
-// Exo 01 
-// Créer une variable contenant 3.7
-// Affiché cette variable arrondi à son entier superieur
-
-let number = 3.7;
-console.log(number);
+// Exo 01
+// Créer une variable du nom de number contenant 3.7
+// Affiché cette variable arrondie à son entier superieur// Exo 02
+// Afficher la variable number à son entier inférieur// Exo 03
+// Créer deux variables number01 et number02
+// L'une valant 4 et l'une valant 7
+// Avec l'aide d'une function retourner le plus grand// Exo 04
+// Avec l'aide d'une function et des variables de l'exercice 03 retourner le plus petit// Exo05
+// Créer trois variables non entier avec comme nom et valeur :
+// nombreNonEntier01 = 54.7
+// nombreNonEntier02 = 147.1
+// nombreNonEntier03 = 14.4
+// Les mettres a leur entier le plus grand et retourner le plus grand
